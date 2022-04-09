87f33c83cfe8a95988e7fe051f9dc6dbeed0a173
cruizfer <celiaruizfer1@gmail.com>
2022-04-09 19:49:19 +0200

    New Feature: home + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
